0eb73a8267e12b7e77154700a027918778b76cd6
DESKTOP-98MUN83\PC <Sittichok.Juns@gmail.com>
2024-10-25 06:57:26 +0700

    add text homepage

--- frontend/src/pages/mainPage/Home.jsx
+++ frontend/src/pages/mainPage/Home.jsx
@@ -8,8 +8,12 @@ function Home() {
       <div className="container-fullid">
         <img src={ banner } className="img-fluid" alt="Banner Image" />
       </div>
-      
-      
+      <div class="d-flex justify-content-center pt-4">
+        <div className="underline-home "></div>
+      </div>
+    
+      <h3 class="pt-2 text-center text-new-dark">ยินดีต้อนรับเข้าสู่เว็บไซต์ "PuglifeHouse"</h3>
+      <h4 class="text-center">เว็บไซต์สำหรับขายสุนัขพันธุ์ปั๊กโดยเฉพาะ</h4>
     </>
   );
 }
