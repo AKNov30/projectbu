088427a15eb0af1d4de39dd6d95755e5a277c305
AKNov30 <amonthjp@gmail.com>
2024-10-24 18:14:11 +0700

    test image

--- frontend/src/components/shopcomponent/DogCardMain.jsx
+++ frontend/src/components/shopcomponent/DogCardMain.jsx
@@ -2,25 +2,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { logo } from '../../assets'; // Default logo image
+import { apiUrl } from '../../config/apiConfig';
+import { formatPrice } from '../../utils/formatPrice';
 
 function DogCardMain({ product, imageUrls }) {
-  // ตรวจสอบว่า imageUrls มีค่าและเป็น array ที่ไม่ว่างเปล่า
-  const validImageUrl = imageUrls.find(url => url && url.startsWith("https://res.cloudinary.com"));
-
   return (
     <div className="col-xl-3 col-lg-6 col-md-6 d-md-flex align-items-stretch pt-3" key={product.dog_id}>
       <Link to={`/detail-dog/${product.dog_id}`} className="card-setting bg-white">
         <div className="d-flex justify-content-center">
-          {validImageUrl ? (
+          {imageUrls.length > 0 ? (
             <img
               className="setting-pic justify-content-center img-fluid"
-              src={validImageUrl} // ใช้ URL ที่ตรวจสอบแล้วว่าถูกต้อง
+              src={`${ apiUrl }${imageUrls[0]}`} // Show the first image from the array
               alt={product.dogs_name}
             />
           ) : (
             <img
               className="setting-pic justify-content-center img-fluid"
-              src={logo} // ใช้รูปสำรองหากไม่มี URL ที่ถูกต้อง
+              src={logo} // Default image
               alt={product.dogs_name}
             />
           )}
@@ -30,11 +29,11 @@ function DogCardMain({ product, imageUrls }) {
           <h5 className="fw-bold pt-3">{product.dogs_name}</h5>
           <p>รหัส : {product.dog_id}</p>
           <p>สี : {product.color}</p>
-          <p>อายุ : {product.age}</p>
-          <p>ราคา : {product.price} THB</p>
+          <p>อายุ : {product.age}</p> {/* Use age property passed */}
+          <p>ราคา : {formatPrice(product.price)} บาท</p>
         </div>
         <div className="d-flex justify-content-center pt-3">
-          <button type="button" className="btn btn-outline-warning setting-btn-reserve">
+          <button type="button" className="btn btn-outline-primary setting-btn-reserve">
             รายละเอียด
           </button>
         </div>
